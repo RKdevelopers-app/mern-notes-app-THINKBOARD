88842ac5d4a3a3d8c41b0b9a83f28b4265b960c2
RKdevelopers-app <gpushkar05@gmail.com>
2025-11-26 15:38:17 +0530

    rate limit updated

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@
 
  const ratelimit = new Ratelimit({
     redis : Redis.fromEnv(),
-    limiter : Ratelimit.slidingWindow(30, "20 s"),
+    limiter : Ratelimit.slidingWindow(5, "100 s"),
  });
 
  export default ratelimit; 
